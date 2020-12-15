72bb7ef5153cdd7e0af033ad78f22f2148ea7e56
Richard Weber <weberrichard92@gmail.com>
2020-12-15 21:21:43 +0100

    adding first test project

---
+++ projects/first_test/first_test.cpp
@@ -0,0 +1,16 @@
+#include "../../source/moc_solver.h"
+
+using namespace std;
+
+int main()
+{
+	string case_folder = "../../models/test/";
+	string case_name = "P045W.csv";
+   moc_solver *patient = new moc_solver(case_folder + case_name, 5);
+   patient->initialization();
+   patient->forward_solver("C");
+   patient->save_results();
+
+   cout << endl << endl;
+   return 0;
+}
