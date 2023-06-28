51e969fa1e83e06bba6e1be130fcf42887afc6db
Richard Weber <weberrichard92@gmail.com>
2023-06-28 14:41:27 +0200

    minor testing

--- projects/test_solver/plot_com.py
+++ projects/test_solver/plot_com.py
@@ -5,16 +5,16 @@ import pandas as pd
 # Reymond_103
 # Reymond_103_Q
 
-cases = ['Reymond_99_heart_ref3','Reymond_99_heart_ref3']
+cases = ['Abel','Abel']
 models = ['arterial','arterial']
 
-elements = ['A5','A5']
+elements = ['A1','A1']
 
 mmHg_to_Pa = 133.3616
 
 start = 1
 
-data = pd.read_csv("results\\" + cases[0] + "\\" + models[0] + "\\" + elements[0] + ".txt",header=None)
+data = pd.read_csv("results\\results_moc\\" + cases[0] + "\\" + models[0] + "\\" + elements[0] + ".txt",header=None)
 t = data[0]
 p = (data[2-start]-1e5)/mmHg_to_Pa;
 v = data[4-start];
@@ -23,11 +23,11 @@ m = data[8-start];
 d = data[10-start];
 a = data[12-start];
 #q = data[2]*1e6
-plt.plot(t,q)
+plt.plot(t,p)
 
-start = 0
+#start = 0
 
-data = pd.read_csv("results\\" + cases[1] + "\\" + models[1] + "\\" + elements[1] + ".txt",header=None)
+data = pd.read_csv("results\\results_maccormack\\" + cases[1] + "\\" + models[1] + "\\" + elements[1] + ".txt",header=None)
 t = data[0]
 p = (data[2-start]-1e5)/mmHg_to_Pa;
 v = data[4-start];
@@ -36,12 +36,12 @@ m = data[8-start];
 d = data[10-start];
 a = data[12-start];
 #q = data[2]*1e6
-plt.plot(t,q)
+plt.plot(t,p)
 
 plt.xlabel('time [s]')
 #plt.ylabel('volume flow rate [ml/s]')
 #plt.ylabel('pressure [mmHg]')
-leg = ['start','end']
+leg = ['moc','maccormack']
 plt.legend(leg)
 plt.grid()
 plt.show()
@@ -53,7 +53,7 @@ plt.show()
 # node_id = "R1"
 # data = pd.read_csv("results\\" + cases[1] + "\\" + model + "\\" + node_id + ".txt",header=None)
 # t = data[0]
-# #p = (data[1]-1e5)/mmHg_to_Pa;
+# #p = (data[1]-1e5)/mmHg_to_Pa;9
 # q = data[1]*1e6
 # plt.plot(t,q)
 # plt.grid()
--- projects/test_solver/test_solver.cpp
+++ projects/test_solver/test_solver.cpp
@@ -6,50 +6,58 @@ using namespace std;
 int main(int argc, char* argv[])
 {
    // basic stuff
-	// string case_folder = "../../models/test_cases/";
+	//string case_folder = "../../models/test_cases/";
    string case_folder = "../../models/";
    double heart_rate = 75.6;  // if there is a heart model   
    double period_time = 60./heart_rate;
 
    // tested models
    vector<string> case_names;
-   // case_names.push_back("v_perif_p0");
-   // case_names.push_back("p0_perif_v");
-   // case_names.push_back("q_perif_p0");
-   // case_names.push_back("p0_perif_q");
-   // case_names.push_back("p_perif_p0");
-   // case_names.push_back("p0_perif_p");
-   // case_names.push_back("p_Re_perif");
-   // case_names.push_back("p_Rs_perif");
-   // case_names.push_back("p1_p0");
-   // case_names.push_back("p_node_perif_p0");
-   // case_names.push_back("p_join_perif_p0");
-   // case_names.push_back("p_0D");
-   // case_names.push_back("p_0D_perif");
-   // case_names.push_back("p_0D_p37");
+   //case_names.push_back("v_perif_p0");
+   //case_names.push_back("p0_perif_v");
+   //case_names.push_back("q_perif_p0");
+   //case_names.push_back("p0_perif_q");
+   //case_names.push_back("p_perif_p0");
+   //case_names.push_back("p0_perif_p");
+   //case_names.push_back("p_Re_perif");
+   //case_names.push_back("p_Rs_perif");
+   //case_names.push_back("p1_p0");
+   //case_names.push_back("p_node_perif_p0");
+   //case_names.push_back("p_join_perif_p0");
+   //case_names.push_back("p_0D");
+   //case_names.push_back("p_0D_perif");
+   //case_names.push_back("p_0D_p37");
    
-   case_names.push_back("Reymond_99_heart_ref3");
+   case_names.push_back("Abel");
 
-   for(int i=0; i<case_names.size(); i++)
+   vector<int> st{0,1};
+   vector<string> sn{"MacCormack","MoC"};
+   vector<string> sf{"results_maccormack","results_moc"};
+   for(int j=1; j<2; j++)
    {
-      cout << " [*] case: " << case_names[i] << endl;
-      // loading original case
-      first_blood *fb = new first_blood(case_folder + case_names[i]);
-      cout << "   + load: OK" << endl;
-
-      //fb->time_end = sim_time;
-      fb->time_period = period_time;
-      fb->is_periodic_run = false;
-      //fb->time_node = "n1";
-    
-      // running the simulation
-      bool is_run_ok = fb->run();
-      cout << "   + run: OK" << endl;
-
-      if(is_run_ok)
+      cout << "[O] SOLVER: " << sn[j] << endl;
+      for(int i=0; i<case_names.size(); i++)
       {
-         fb->save_results();
-         cout << "   + save: OK" << endl;
+         cout << " [*] case: " << case_names[i] << endl;
+         // loading original case
+         first_blood *fb = new first_blood(case_folder + case_names[i]);
+         cout << "   + load: OK" << endl;
+
+         //fb->time_end = sim_time;
+         fb->time_period = period_time;
+         fb->is_periodic_run = false;
+         fb->solver_type = st[j];
+         //fb->time_node = "n1";
+       
+         // running the simulation
+         bool is_run_ok = fb->run();
+         cout << "   + run: OK" << endl;
+
+         if(is_run_ok)
+         {
+            fb->save_results(sf[j] + "/" + case_names[i]);
+            cout << "   + save: OK" << endl;
+         }
       }
    }
 
