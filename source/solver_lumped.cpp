f156b3c54580e2835c35e549b2606dc4eded4deb
Richard Weber <weberrichard92@gmail.com>
2026-01-16 11:35:34 +0100

    Update solver_lumped.cpp

--- source/solver_lumped.cpp
+++ source/solver_lumped.cpp
@@ -633,7 +633,5 @@ vector<double> solver_lumped::p_drop_q(double q, int index) {
         slope = (dp[i+1] - dp[i]) / (qv[i+1] - qv[i]);
         p_val = dp[i] + slope * (q - qv[i]);
     }
-    cout << "q: " << q << " p: " << p_val << endl;
-    cin.get();
     return {p_val, slope};
 }
